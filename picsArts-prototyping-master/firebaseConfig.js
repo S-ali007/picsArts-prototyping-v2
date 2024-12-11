db78871e27b07f766972812df2e28ab61195dc30
ali-techtribe <dmnzampa@gmail.com>
2024-12-11 21:58:25 +0500

    reverted changes and privacy issues

--- picsArts-prototyping-master/firebaseConfig.js
+++ picsArts-prototyping-master/firebaseConfig.js
@@ -9,16 +9,24 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA-ec5iWV1lI7drrBf9R2yliG_Z__HVvF8",
+//   authDomain: "techart-f13ea.firebaseapp.com",
+//   projectId: "techart-f13ea",
+//   storageBucket: "techart-f13ea.firebasestorage.app",
+//   messagingSenderId: "31861485972",
+//   appId: "1:31861485972:web:55d793173c7597a82a9f81",
+//   measurementId: "G-F95XMDCHWP",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyA-ec5iWV1lI7drrBf9R2yliG_Z__HVvF8",
-  authDomain: "techart-f13ea.firebaseapp.com",
-  projectId: "techart-f13ea",
-  storageBucket: "techart-f13ea.firebasestorage.app",
-  messagingSenderId: "31861485972",
-  appId: "1:31861485972:web:55d793173c7597a82a9f81",
-  measurementId: "G-F95XMDCHWP",
+  apiKey: "AIzaSyB09-xmV1CoY7LqvXV88DatCHDsFAWgoLI",
+  authDomain: "genart-1b140.firebaseapp.com",
+  projectId: "genart-1b140",
+  storageBucket: "genart-1b140.firebasestorage.app",
+  messagingSenderId: "389285846949",
+  appId: "1:389285846949:web:2723a53206d38a69aed24c",
+  measurementId: "G-4TYETWZ75B"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const provider = new GoogleAuthProvider(firebaseConfig);
